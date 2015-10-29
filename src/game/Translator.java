b46f8291bb88585eae65c28d517d12526285778b
Hjorthenify <Unknown>
2015-10-29 10:50:14 +0100

    Added translator, which will be used for localization support.

---
+++ src/game/Translator.java
@@ -0,0 +1,26 @@
+package game;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Translator {
+	static ResourceBundle strings;
+	public static String setString(String keyword)
+	{
+		//If not previous set, use default locale(da, DK)
+		if(strings==null)
+		{
+			setLocale(new Locale("da", "DK"));
+		}
+		return strings.getString(keyword);
+	}
+	public static void setLocale(Locale l)
+	{
+		strings = ResourceBundle.getBundle("MessageBundle", l);
+	}
+	//Avoids creating objects of this class
+	private Translator()
+	{
+		
+	}
+}
