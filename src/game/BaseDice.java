8b7902c52acc3091a701ee4d1ad45997e46c7cbe
Thomas Bech Madsen <thomasbm@live.dk>
2015-10-29 12:03:34 +0100

    Har lavet BaseDice abstract DicePair har dermed modtaget funktionerne

--- src/game/BaseDice.java
+++ src/game/BaseDice.java
@@ -2,23 +2,9 @@ package game;
 import java.util.Random;
 
 
-public class BaseDice {
-	int result;
-	int firstDie;
-	int secondDie;
-	Random psudoGen = new Random(System.currentTimeMillis());
+public abstract class BaseDice {
 
-
+	public abstract void rollDice();
 	
-	
-	public void rollDice()
-	{
-		firstDie = psudoGen.nextInt(6)+1; //Returns a number between 1 and 6 since 6 is exclusive. 
-		secondDie = psudoGen.nextInt(6)+1;
- 	}
-	
-	public int result(){
-		result = firstDie + secondDie;
-		return result;
-	}
+	public abstract int result();
 }
