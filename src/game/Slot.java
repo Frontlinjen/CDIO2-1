d75352781c3cdbf6fa99045781643f3244164f15
SenadBegovic <senadbegovic2500@hotmail.com>
2015-10-29 12:19:03 +0100

    Oprettede klassen Slot
    Samt oprettede forskellige variabler og metoder i klassen

---
+++ src/game/Slot.java
@@ -0,0 +1,21 @@
+package game;
+
+public class Slot {
+
+	private int value;
+	private String name;
+	private String description;
+	
+	
+	public int getValue() {
+		return value;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	
+	
+}
