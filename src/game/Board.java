051be5d4766f1f3b4f1be8e4f375419ab3b73b40
Hjorthenify <Unknown>
2015-10-29 12:04:16 +0100

    Added initializeBoard

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,8 +1,36 @@
 package game;
 
+import java.awt.Color;
+
+import desktop_fields.Field;
+import desktop_fields.Tax;
+import desktop_resources.GUI;
+
 public class Board {
+	private void initializeBoard()
+    {
+            Field[] fields = new Field[11];
+            for (int i = 0; i < fields.length; i++) {
+                    Color col;
+                    if (i%2 == 0)
+                    {
+                            col = Color.pink;
+                    }
+                    else
+                    {
+                            col = Color.magenta;
+                    }
+                    Tax t = new Tax.Builder().setBgColor(col).build();
+                    fields[i] = t;
+            }
+            GUI.create(fields);
+           
+           
+    }
 	public void startGame()
 	{
+		initializeBoard();
+		GUI.showMessage("Hi");
 		//Write logic here!
 	}
 	public static void main(String[] args) {
